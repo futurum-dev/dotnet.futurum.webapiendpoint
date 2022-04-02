6aea7a7b342fcb096f6071ded3f402a73e18395a
futurum-dev <me@futurum.dev>
2022-04-02 19:13:40 +0100

    MapFromRequestMultipartMapper improvements

---
+++ src/Futurum.WebApiEndpoint/RequestMapFromMultipart.cs
@@ -0,0 +1,6 @@
+namespace Futurum.WebApiEndpoint;
+
+/// <summary>
+/// Request dto for MapFromMultipart
+/// </summary>
+public record RequestMapFromMultipartDto<TRequestDto> : IRequestWrapperDto<TRequestDto>;
